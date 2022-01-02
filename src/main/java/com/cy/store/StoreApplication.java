ab7f5e540915e6fc1b8ca309484a098a9f6366e3
Jacobwsw <1753558200@qq.com>
2022-01-02 20:12:58 +0800

    头像上传优化

--- src/main/java/com/cy/store/StoreApplication.java
+++ src/main/java/com/cy/store/StoreApplication.java
@@ -3,6 +3,9 @@ package com.cy.store;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
 
 @SpringBootApplication
 @MapperScan("com.cy.store.mapper")
